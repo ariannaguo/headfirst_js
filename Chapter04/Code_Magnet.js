daabb66ea22495849694dba9eedd00e53ac75b73
ariannaguo <ariannaguo@gmail.com>
2014-07-18 15:17:40 +0800

    for loop

--- Chapter04/Code_Magnet.js
+++ Chapter04/Code_Magnet.js
@@ -11,3 +11,12 @@ while (i<hasBubbleGum.length){
     }
     i=i+1;
 }
+
+//for loop
+var products = ["Choo Choo Chocolate","Icy Mint","Cake Batter","Bubblegum"];
+var hasBubbleGum =[false,false,false,true];
+for (i=0;i<hasBubbleGum;i=i+1) {
+    if (hasBubbleGum[i]) {
+        console.log(products[i] + " contains bubble gum");
+    }
+}
