7105c6cbeb1becb1ca37e0a0da9ce7c2689be533
ariannaguo <ariannaguo@gmail.com>
2014-07-19 11:56:36 +0800

    comments added

--- Chapter04/bubble.js
+++ Chapter04/bubble.js
@@ -8,34 +8,36 @@ var scores = [60, 50, 60, 58, 54, 54,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
 var output;
-var highScore=0;
-for(i=0;i<scores.length;i++){
-    output="Bubble solution #"+i+" socore: "+scores[i];
+var highScore = 0;
+for (i = 0; i < scores.length; i++) {
+    output = "Bubble solution #" + i + " score: " + scores[i];
     console.log(output);
-    if(scores[i]>highScore){
-        highScore=scores[i];
-        //why not return highScore? 没有return,后面为什么可以直接用？
+    if (scores[i] > highScore) {
+        highScore = scores[i];
+        //why not return highScore? only function has "return"
     }
 }
-console.log("Bubble tests: "+scores.length);
-console.log("Highest bubble score: "+highScore);
-//console.log("Solutions with highest score: #"+i);      36... where does it come? because i is local variable?
+console.log("Bubble tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+//console.log("Solutions with highest score: #"+i);  i=36... only function has local variable, here i is a global variable; 循环的终点其实是scores.length
 
 //empty Array
-var bestSolutions=[];
-for(var i=0;i<scores.length;i++){
-     if(scores[i]==highScore){
-         bestSolutions.push(i)
-     }
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions.push(i)
     }
-console.log("Solutions with the highest score: "+bestSolutions);
-//if push does not exist???
-var bestSolutions=[];
-for(var i=0;i<scores.length;i++){
-    if(scores[i]==highScore){
-        console.log(bestSolutions[i]);
+}
+console.log("Solutions with the highest score: " + bestSolutions);
+
+//if push does not exist?
+var bestSolutions = [];
+for (var i = 0; i < scores.length; i++) {
+    if (scores[i] == highScore) {
+        bestSolutions[bestSolutions.length] = i; //why not bestSolutions[bestSolutions.length-1] = i
     }
-    }
+}
+console.log("Solutions with the highest score: " + bestSolutions);
 
 //=====================Rewrite with function========================
 var scores = [60, 50, 60, 58, 54, 54,
@@ -44,29 +46,33 @@ var scores = [60, 50, 60, 58, 54, 54,
     69, 64, 66, 55, 52, 61,
     46, 31, 57, 52, 44, 18,
     41, 53, 55, 61, 51, 44];
-function printAndGetHighScore(scores){
+
+function printAndGetHighScore(scores) {
     var output;
-    var highScore=0;
-    for(i=0;i<scores.length;i++){
-        output="Bubble solution #"+i+" socore: "+scores[i];
+    var highScore = 0;
+    for (i = 0; i < scores.length; i++) {
+        output = "Bubble solution #" + i + " score: " + scores[i];
         console.log(output);
-        if(scores[i]>highScore) {
+        if (scores[i] > highScore) {
             highScore = scores[i];
         }
-}
-return highScore;
     }
-var highScore = printAndGetHighScore(scores); //why?
-console.log("Bubble tests: "+scores.length);
-console.log("Highest bubble score: "+highScore);
-function getBestResults(scores,highScore){
-    var bestSolutions=[];
-    for(var i=0;i<scores.length;i++){
-        if(scores[i]==highScore){
-            bestSolutions.push(i)
+    return highScore;
+}
+
+var highScore = printAndGetHighScore(scores);
+// the highScore here is a new global variable...has nothing to do with the upper highScore.
+
+console.log("Bubble tests: " + scores.length);
+console.log("Highest bubble score: " + highScore);
+function getResults(scores, score) {
+    var solutions = [];
+    for (var i = 0; i < scores.length; i++) {
+        if (scores[i] == score) {
+            solutions.push(i);
         }
     }
-    return bestSolutions;
+    return solutions;
 }
-var bestSolutions=getBestResults(scores,highScore);
-console.log("Solutions with the highest score: "+bestSolutions);
+var bestSolutions = getResults(scores, highScore);
+console.log("Solutions with the highest score: " + bestSolutions);
