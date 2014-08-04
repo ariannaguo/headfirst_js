ce9ccc9bd3f63b79c24918aa9508a091bb3dd038
ariannaguo <ariannaguo@gmail.com>
2014-08-05 00:45:57 +0800

    view+model

---
+++ Chapter08/battleship.js
@@ -0,0 +1,78 @@
+/**
+ * Created by Arianna on 7/30/14.
+ */
+var view = {
+    displayMessage: function (msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+};
+
+//view.displayHit("00");
+//view.displayMiss("34");
+//
+//view.displayMessage("Tap tap, is this thing on?")
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [
+        {locations: ["06", "07", "08"], hits: ["", "", ""]},
+        {locations: ["24", "34", "44"], hits: ["", "", ""]},
+        {locations: ["10", "11", "12"], hits: ["", "", ""]}
+    ],
+    fire: function (guess) {
+        for (i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+//            var locations=ship.locations;
+//            var index=locations.indexOf(guess);
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");//not very clear why isSunk should be placed here.
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+    isSunk: function (ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+model.fire("53");
+
+model.fire("06");
+model.fire("16");
+model.fire("26");
+
+model.fire("34");
+model.fire("24");
+model.fire("44");
+
+model.fire("12");
+model.fire("11");
+model.fire("10");
